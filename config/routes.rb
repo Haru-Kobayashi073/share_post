93e7e9d8bbbe7e964d0c766856b5ebda842ea750
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-06-07 11:50:13 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :articles, only: %i[index show new edit create update]
+  resources :articles, only: %i[index show new edit create update destroy]
 
 end
